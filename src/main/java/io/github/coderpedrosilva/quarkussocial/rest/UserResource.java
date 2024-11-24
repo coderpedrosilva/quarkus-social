f84dca1178da615f5710836584e6db0d999092db
pedro <progerios@hotmail.com>
2024-11-24 13:34:45 -0300

    creating user POST

---
+++ src/main/java/io/github/coderpedrosilva/quarkussocial/rest/UserResource.java
@@ -0,0 +1,17 @@
+package io.github.coderpedrosilva.quarkussocial.rest;
+
+import io.github.coderpedrosilva.dto.CreateUserRequest;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users")
+public class UserResource {
+
+    @POST
+    public Response createUser( CreateUserRequest userRequest ) {
+        return Response.ok().build();
+    }
+
+}
